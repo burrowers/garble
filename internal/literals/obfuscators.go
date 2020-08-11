19afa8f8d07a9650a97d80e9ebb10fadafbc8aa5
Pagran <67878280+pagran@users.noreply.github.com>
2020-08-11 18:45:03 +0300

    Add operator randomizer for xor obfuscator

--- internal/literals/obfuscators.go
+++ internal/literals/obfuscators.go
@@ -4,6 +4,7 @@ import (
 	cryptrand "crypto/rand"
 	"fmt"
 	"go/ast"
+	"go/token"
 	mathrand "math/rand"
 	"os"
 	"strings"
@@ -50,4 +51,43 @@ func genRandIntSlice(max, count int) []int {
 		indexes[i] = mathrand.Intn(max)
 	}
 	return indexes
+}
+
+var allOperators = []token.Token{token.XOR, token.ADD, token.SUB}
+
+func genRandOperator() token.Token {
+	return allOperators[mathrand.Intn(len(allOperators))]
+}
+
+func evalOperator(t token.Token, x, y byte) byte {
+	switch t {
+	case token.XOR:
+		return x ^ y
+	case token.ADD:
+		return x + y
+	case token.SUB:
+		return x - y
+	default:
+		panic("unknown operator")
+	}
+}
+
+func getReversedOperator(t token.Token, x, y ast.Expr) *ast.BinaryExpr {
+	expr := &ast.BinaryExpr{
+		X: x,
+		Y: y,
+	}
+
+	switch t {
+	case token.XOR:
+		expr.Op = token.XOR
+	case token.ADD:
+		expr.Op = token.SUB
+	case token.SUB:
+		expr.Op = token.ADD
+	default:
+		panic("unknown operator")
+	}
+
+	return expr
 }
