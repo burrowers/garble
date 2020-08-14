ea51e78283206108e00bdccf9d50fffcef703004
lu4p <lu4p@pm.me>
2020-08-14 21:24:15 +0200

    Check that all files use LF line endings in CI

---
+++ crlf_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if \
+	grep \
+		--recursive \
+		--files-with-matches \
+		--binary \
+		--binary-files=without-match \
+		--max-count=1 \
+		--exclude-dir="\.git" \
+		$'\r' \
+		. \
+	; then
+	# TODO exit status should be number of files with wrong endings found
+	echo -e "Found at least a file with CRLF endings."
+	exit 1
+fi
+
+echo -e "No files with CRLF endings found."
+exit 0
