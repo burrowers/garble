fe0def8db9904f3630c9ffd10c2fe4dec6c56fdf
lu4p <lu4p@pm.me>
2020-08-14 22:04:06 +0200

    Fix

--- crlf_test.sh
+++ crlf_test.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
-echo "searching for CRLF endings in: ."
-
-BOLD_RED='\033[1;31m'
-BOLD_GREEN='\033[1;32m'
-NC='\033[0m'
-
-ERROR_COUNT=0
-
 if \
-  grep \
-    --recursive \
-    --files-with-matches \
-    --binary \
-    --binary-files=without-match \
-    --max-count=1 \
-    --exclude-dir="\.git" \
-    $'\r' \
-    . \
-  ; then
-  # TODO exit status should be number of files with wrong endings found
-  echo -e "${BOLD_RED}Found at least a file with CRLF endings.${NC}"
-  exit 1
+	grep \
+		--recursive \
+		--files-with-matches \
+		--binary \
+		--binary-files=without-match \
+		--max-count=1 \
+		--exclude-dir="\.git" \
+		$'\r' \
+		. \
+	; then
+	# TODO exit status should be number of files with wrong endings found
+	echo -e "Found at least a file with CRLF endings."
+	exit 1
 fi
 
-echo -e "${BOLD_GREEN}No files with CRLF endings found.${NC}"
+echo -e "No files with CRLF endings found."
 exit 0
