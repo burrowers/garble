bd46c2938038cfa5f34b64d2c42cb963524325c9
pagran <67878280+pagran@users.noreply.github.com>
2020-08-25 18:22:53 +0300

    add blacklist for runtime std packages

--- main.go
+++ main.go
@@ -578,11 +578,16 @@ func transformCompile(args []string) ([]string, error) {
 	return append(flags, newPaths...), nil
 }
 
+const privateBlacklist = "runtime,internal/cpu,internal/bytealg"
+
 // isPrivate checks if GOPRIVATE matches path.
 //
 // To allow using garble without GOPRIVATE for standalone main packages, it will
 // default to not matching standard library packages.
 func isPrivate(path string) bool {
+	if GlobsMatchPath(privateBlacklist, path) {
+		return false
+	}
 	if path == "main" || path == "command-line-arguments" || strings.HasPrefix(path, "plugin/unnamed") {
 		// TODO: why don't we see the full package path for main
 		// packages? The linker has it at the top of -importcfg, but not
