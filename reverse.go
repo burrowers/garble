93d88a9e712ab3251b5144ccdd4996ee3d19586e
Daniel Martí <mvdan@mvdan.cc>
2022-06-18 11:52:34 +0100

    actually remove temporary directories after obfuscation
    Back in February 2021, we changed the obfuscation logic so that the
    entire `garble build` process would use one shared temporary directory
    across all package builds, reducing the amount of files we created in
    the top-level system temporary directory.
    
    However, we made one mistake: we didn't swap os.Remove for os.RemoveAll.
    Ever since then, we've been leaving temporary files behind.
    
    Add regression tests, which failed before the fix, and fix the bug.
    Note that we need to test `garble reverse` as well, as it calls
    toolexecCmd separately, so it needs its own cleanup as well.
    
    The cleanup happens via the env var, which doesn't feel worse than
    having toolexecCmd return an extra string or cleanup func.
    
    While here, also test that we support TMPDIRs with special characters.

--- reverse.go
+++ reverse.go
@@ -44,7 +44,9 @@ One can reverse a captured panic stack trace as follows:
 	listArgs = append(listArgs, pkg)
 	// TODO: We most likely no longer need this "list -toolexec" call, since
 	// we use the original build IDs.
-	if _, err := toolexecCmd("list", listArgs); err != nil {
+	_, err := toolexecCmd("list", listArgs)
+	defer os.RemoveAll(os.Getenv("GARBLE_SHARED"))
+	if err != nil {
 		return err
 	}
 
